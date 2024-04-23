4d05d0fe09744500cf00710cca2d709b7112b94a
Harper Nordin <harper.nordin@gmail.com>
2024-04-22 21:53:25 -0700

    Created loader and began parsing the prompts into answers

--- load_players.py
+++ load_players.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
 
 hart_index = "hart_winner_name"
@@ -55,6 +56,8 @@ hockey_teams = {
         'winnipegjets': 'WPG'
         }
 
+categories = dict()
+
 
 def get_teams():
     team_list = [hockey_teams[i] for i in list(hockey_teams)]
@@ -63,17 +66,254 @@ def get_teams():
         link = f'https://www.hockey-reference.com/teams/{team}/skaters.html'
         page = requests.get(link).text
         soup = BeautifulSoup(page, 'html.parser')
-
-        players = soup.find_all('tr', attrs={'class': False})
+        table = soup.find('tbody')
+        players = table.find_all('tr', attrs={'class': False})
         player_list = []
+
         for player in players:
             stats = player.findChildren()
             name = stats[1].string
             start = stats[3].string
             end = stats[4].string
             data = (name, start, end)
-            print(data)
             player_list.append(data)
 
+        categories[team] = player_list
+        time.sleep(3.1)
 
-get_teams()
+    for team in team_list:
+        link = f'https://www.hockey-reference.com/teams/{team}/draft.html'
+        page = requests.get(link).text
+        soup = BeautifulSoup(page, 'html.parser')
+
+        table = soup.find('tbody')
+        players = table.find_all('tr', attrs={'class': False})
+        player_list = []
+
+        for player in players:
+            stats = player.findChildren(recursive=False)
+            if stats[1].string == '1':
+                name = stats[3].string
+                player_list.append(data)
+
+        categories['First Round ' + team] = player_list
+        time.sleep(3.1)
+
+
+def get_season_leads(key, link):
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    table = soup.find('tbody')
+    rows = table.find_all('tr', attrs={'class': False})
+
+    player_list = []
+    for row in rows:
+        stats = row.findChildren(recursive=False)
+        name = stats[1].find('a').string
+        start = stats[3].string[:4]
+        end = str(int(start) + 1)
+        data = (name, start, end)
+        player_list.append(data)
+
+    categories[key] = player_list
+
+
+def get_career_leads(key, link):
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    table = soup.find('tbody')
+    rows = table.find_all('tr', attrs={'class': False})
+
+    player_list = []
+    for row in rows:
+        stats = row.findChildren()
+        name = stats[1].find('a').string
+        player_list.append(name)
+
+    categories[key] = player_list
+
+
+def get_hof():
+    link = 'https://www.hockey-reference.com/awards/hhof.html'
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    table = soup.find('tbody')
+    rows = table.find_all('tr', attrs={'class': False})
+
+    player_list = []
+    for row in rows:
+        stats = row.findChildren()
+        name = stats[1].string
+        player_list.append(name)
+
+    categories['Hall of Fame'] = player_list
+
+
+def get_season_trophies(key, link):
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    table = soup.find('tbody')
+    rows = table.find_all('tr', attrs={'class': False})
+
+    player_list = []
+    for row in rows:
+        stats = row.findChildren(recursive=False)
+        name = stats[2].string
+        team = stats[4].string
+        data = (name, team)
+        player_list.append(data)
+
+    categories[key] = player_list
+
+
+def get_birth_places():
+    link = 'https://www.hockey-reference.com/friv/birthplaces.cgi'
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+
+    div = soup.find('div', attrs={'id': 'birthplace_3'})
+    p_list = div.findChildren(recursive=False)
+    count_a_list = [p.find('a') for p in p_list[1:]]
+    count_links = [a['href'] for a in count_a_list]
+
+    base_link = 'https://www.hockey-reference.com'
+
+    for link in count_links:
+        new_page = requests.get(base_link + link).text
+        new_soup = BeautifulSoup(new_page, 'html.parser')
+
+        table = new_soup.find('tbody')
+        rows = table.find_all('tr', attrs={'class': False})
+
+        player_list = []
+        for row in rows:
+            stats = row.findChildren(recursive=False)
+            name = stats[1].string
+            player_list.append(name)
+
+        time.sleep(3.1)
+
+    categories['Born Outside North America'] = player_list
+
+    # Get Canadian Players
+    div = soup.find('div', attrs={'id': 'birthplace_1'})
+    p_list = div.findChildren(recursive=False)
+    prov_a_list = [p.find('a') for p in p_list[1:]]
+    prov_links = [a['href'] for a in prov_a_list]
+
+    for link in prov_links:
+        new_page = requests.get(base_link + link).text
+        new_soup = BeautifulSoup(new_page, 'html.parser')
+
+        table = new_soup.find('tbody')
+        rows = table.find_all('tr', attrs={'class': False})
+
+        player_list = []
+        for row in rows:
+            stats = row.findChildren(recursive=False)
+            name = stats[1].string
+            player_list.append(name)
+
+        time.sleep(3.1)
+
+    categories['Born Outside US 50 States and DC'] = player_list
+
+    # You need to do out of usa you stupid goof
+
+
+def main():
+    goals_link = 'https://www.hockey-reference.com/leaders/goals_season.html'
+    points_link = 'https://www.hockey-reference.com/leaders/points_season.html'
+    w_link = 'https://www.hockey-reference.com/leaders/wins_goalie_season.html'
+
+    print("    >Geting 40+ goal scorers...(1/16)\n")
+    get_season_leads('40+ Goalsseason', goals_link)
+    time.sleep(3.1)
+
+    print("    >Geting 100+ point scorers...(2/16)\n")
+    get_season_leads('100+ Pointsseason', points_link)
+    time.sleep(3.1)
+
+    print("    >Geting 30+ win goalies...(3/16)\n")
+    get_season_leads('30+ Winsseason', w_link)
+    time.sleep(3.1)
+
+    goals_link = 'https://www.hockey-reference.com/leaders/goals_career.html'
+    points_link = 'https://www.hockey-reference.com/leaders/points_career.html'
+    w_link = 'https://www.hockey-reference.com/leaders/wins_goalie_career.html'
+
+    print("    >Geting 500+ goal career...(4/16)\n")
+    get_career_leads('500+ Goalscareer', goals_link)
+    time.sleep(3.1)
+
+    print("    >Geting 1000+ point career...(5/16)\n")
+    get_career_leads('1000+ Pointscareer', points_link)
+    time.sleep(3.1)
+
+    print("    >Geting 300+ win career...(6/16)\n")
+    get_career_leads('300+ Winscareer', w_link)
+    time.sleep(3.1)
+
+    byng_link = 'https://www.hockey-reference.com/awards/byng.html'
+    ross_link = 'https://www.hockey-reference.com/awards/ross.html'
+    hart_link = 'https://www.hockey-reference.com/awards/hart.html'
+    calder_link = 'https://www.hockey-reference.com/awards/calder.html'
+    vezina_link = 'https://www.hockey-reference.com/awards/vezina.html'
+    norris_link = 'https://www.hockey-reference.com/awards/norris.html'
+    smythe_link = 'https://www.hockey-reference.com/awards/smythe.html'
+
+    print("    >Geting Lady Byng Trophy winners...(7/16)\n")
+    get_season_trophies('Lady Byng Trophy', byng_link)
+    time.sleep(3.1)
+
+    print("    >Geting Art Ross Trophy winners...(8/16)\n")
+    get_season_trophies('Art Ross Trophy', ross_link)
+    time.sleep(3.1)
+
+    print("    >Geting Hart Trophy winners...(9/16)\n")
+    get_season_trophies('Hart Trophy', hart_link)
+    time.sleep(3.1)
+
+    print("    >Geting Calder Trophy winners...(10/16)\n")
+    get_season_trophies('Calder Trophy', calder_link)
+    time.sleep(3.1)
+
+    print("    >Geting Vezina Trophy winners...(11/16)\n")
+    get_season_trophies('Vezina Trophy', vezina_link)
+    time.sleep(3.1)
+
+    print("    >Geting Norris Trophy winners...(12/16)\n")
+    get_season_trophies('Norris Trophy', norris_link)
+    time.sleep(3.1)
+
+    print("    >Geting Conn Smythe Trophy winners...(13/16)\n")
+    get_season_trophies('Conn Smythe Trophy', smythe_link)
+    time.sleep(3.1)
+
+    print("    >Geting all players and first round draft picks...(14/16)\n")
+    get_teams()
+    time.sleep(3.1)
+
+    print("    >Getting all hall of fame winners...(15/16)\n")
+    get_hof()
+    time.sleep(3.1)
+
+    print("    >Geting birthplace data...(16/16)\n")
+    get_birth_places()
+    time.sleep(3.1)
+
+    print("    >Serializing data...")
+    data = json.dumps(categories, indent=2)
+
+    print("    >Writing to file...")
+    with open('data.json', 'w') as outfile:
+        outfile.write(data)
+
+    print("Data written to file, complete")
+
+
+main()
--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
 url = 'https://www.immaculategrid.com/hockey'
-
-hart_index = "hart_winner_name"
-vezina_index = "vezina_winner_name"
-calder_index = "calder_winner_name"
-norris_index = "norris_winner_name"
-smythe_index = "smythe_winner_name"
-
-goal_leader_id = 'leaders_goals'
-point_leader_id = 'leaders_points'
-wins_leader_id = 'leaders_wins_goalie'
-assists_leader_id = 'leaders_assists'
-hall_of_fame = []
-hart = []
-vezina = []
-calder = []
-norris = []
-smythe = []
 
 hockey_teams = {
         'anaheimducks': 'ANA',
@@ -92,31 +76,51 @@ def get_categories(html) -> tuple[list[str], list[str]]:
     return (categories_top, categories_side)
 
 
-def print_answers(top: list[str], side: list[str], answers: list[list[str]]):
+def print_answers(top: list[str], side: list[str], ans: list[list[str]]):
     print(f'        | {top[0]} | {top[1]} | {top[2]}')
     for i in range(3):
-        print(f'{side[i]} | {answers[i][0]} | {answers[i][1]} | {answers[i][2]}')
+        print(f'{side[i]} | {ans[i][0]} | {ans[i][1]} | {ans[i][2]}')
+
+
+def get_players():
+    data = None
+    with open('data.json', 'r') as data_file:
+        data = json.load(data_file)
+
+    return data
 
 
 def main():
-
     (top, side) = get_categories(soup)
     print(top, side)
+
+    start = time.time()
+    player_dict = get_players()
+    d_time = time.time() - start
+    print(f'It took {d_time}ms to load all player data')
+
     used_names = []
-    next_name = ''
     answers = [['' for i in range(3)] for i in range(3)]
 
-
-
-
-    #Max of strings returns the shorter one (usually the team tricode) while the min does the inverse
     for i, row in enumerate(side):
         for j, column in enumerate(top):
-            answers[i][j] = next_name
+            next_name = 'None'
+            if len(row) == len(column) and len(row) == 3:
+                # Just use names
+                list_r = player_dict[row]
+                print(row, list_r[0])
 
+                list_c = [i[0] for i in player_dict[column]]
+                for player in list_r:
+                    if player[0] in list_c:
+                        next_name = player[0]
+                        break
+
+            answers[i][j] = next_name
             used_names.append(next_name)
+
     print()
     print_answers(top, side, answers)
 
-main()
 
+main()
